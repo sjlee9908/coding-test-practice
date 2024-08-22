133cf499f5f94ebd81cf79b8be5ffd58ffef5fd9
lee <sjlee9908@naver.com>
2024-08-22 17:20:18 +0900

    Update 1766.py

--- 백준/1766.py
+++ 백준/1766.py
@@ -3,45 +3,34 @@ import sys
 
 N, M = map(int, sys.stdin.readline().split())
 graph = [[] for _ in range(N+1)]
-parent = [0] * (N + 1)
+parent = [0] * (N + 1) #먼저 풀어야할 문제의 수
 for _ in range(M):
     A, B = map(int, sys.stdin.readline().split())
-    graph[A].append(B)
+    graph[A].append(B) #먼저 풀 문제 -> 나중에 풀 문제
     parent[B] += 1
 
-# def topology_sort():
-#     problem = []
-#     solved = [False] * (N + 1)
-#     i = 1
-#     while len(problem) != N:
-#         if parent[i] == 0 and solved[i] == False:
-#             problem.append(i)
-#             solved[i] = True
-#             for child in graph[i]:
-#                 parent[child] -= 1
-#             i = 0
-#
-#         i += 1
-#         i %= (N+1)
-#
-#
-#     return problem
-
+#위상정렬
 def topology_sort():
+    #방문할 노드들이 저장된 heap
     que = []
 
+    #먼저 방문할 노드들을 heap에 넣음
     for i in range(1, N+1):
+        #먼저 풀어야할 문제의 수가 0이면 먼저 방문
         if parent[i] == 0:
             heapq.heappush(que, i)
 
+    #que가 빌 때까지 반복
     while len(que) != 0:
+        #방문
         v = heapq.heappop(que)
         print(v, end = " ")
 
+        # 나중에 풀 문제의 parent 감소 & heap에다가 넣기
         for child in graph[v]:
             parent[child] -= 1
             if parent[child] == 0:
                 heapq.heappush(que, child)
 
 
-print(*topology_sort())
+topology_sort()
