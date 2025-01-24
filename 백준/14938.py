1ad47eff954fb63061f93af84ae4626cbc433fd0
sjlee9908 <sjlee9908@naver.com>
2025-01-24 15:35:31 +0900

    dd

--- 백준/14938.py
+++ 백준/14938.py
@@ -1,37 +1,33 @@
 import sys
 
-N, M, R = map(int, sys.stdin.readline().split())
-T = list(map(int, sys.stdin.readline().split()))
-graph = [[sys.maxsize] * N for _ in range(N)]
+n, m, r = map(int, sys.stdin.readline().split())
+t = list(map(int, sys.stdin.readline().split()))
+graph = [[sys.maxsize] * n for _ in range(n)]
 
-for _ in range(R):
-    A, B, L = map(int, sys.stdin.readline().split())
-    graph[A-1][B-1] = L
-    graph[B-1][A-1] = L
+for _ in range(r):
+    a, b, l = map(int, sys.stdin.readline().split())
+    graph[a - 1][b - 1] = l
+    graph[b - 1][a - 1] = l
 
-def fw():
-    for k in range(N):
-        for i in range(N):
-            for j in range(N):
-                if graph[i][j] >= graph[i][k] + graph[k][j]:
-                    graph[i][j] = graph[i][k] + graph[k][j]
+for k in range(n):
+    for i in range(n):
+        for j in range(n):
+            graph[i][j] = min(graph[i][k] + graph[k][j], graph[i][j])
 
-for i in range(N):
-    for j in range(N):
-        if i == j: graph[i][i] = 1
-        else:
-            if graph[i][j] <= M: graph[i][j] = 1
-            else: graph[i][j] = 0
+maxt = t[:]
+for i in range(n):
+    for j in range(n):
+        if i == j : continue
+        if graph[i][j] <= m:
+            maxt[i] += t[j]
 
-v = 0
-for i in range(N):
-    tmp = 0
-    for j in range(N):
-        if graph[i][j] == 1:
-            tmp += T[j]
-    v = max(tmp, v)
+# print(*graph)
+# print(maxt)
+print(max(maxt))
 
-print(v)
+
+
+
 
 
 
